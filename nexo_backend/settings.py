c3f4b9b42b5ae38183a3450129b7eb1ffc2db5e8
Carla Noguera <carla.noguera@digisalud.org>
2026-06-28 10:49:35 -0400

    Agregar

--- nexo_backend/settings.py
+++ nexo_backend/settings.py
@@ -1,14 +1,52 @@
-import os
 from pathlib import Path
 
+import environ
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
-SECRET_KEY = "nexo-dev-secret-key-cambiar-en-produccion"
+GDAL_LIBRARY_PATH = os.environ.get(
+    "GDAL_LIBRARY_PATH",
+    r"C:\OSGeo4W\bin\gdal308.dll",
+)
 
-DEBUG = True
+GEOS_LIBRARY_PATH = os.environ.get(
+    "GEOS_LIBRARY_PATH",
+    r"C:\OSGeo4W\bin\geos_c.dll",
+)
+env = environ.Env(
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(list, []),
+    CSRF_TRUSTED_ORIGINS=(list, []),
+    CORS_ALLOWED_ORIGINS=(list, []),
+    DB_NAME=(str, "nexo"),
+    DB_USER=(str, "nexo"),
+    DB_PASSWORD=(str, ""),
+    DB_HOST=(str, "localhost"),
+    DB_PORT=(str, "5432"),
+    NEXO_DB_ENGINE=(str, "postgis"),
+)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
+env.read_env(BASE_DIR / ".env")
 
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+
+CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS", default=[])
+CORS_ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS", default=[])
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=not DEBUG)
+SESSION_COOKIE_SECURE = env.bool("SESSION_COOKIE_SECURE", default=not DEBUG)
+CSRF_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", default=not DEBUG)
+
+SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", default=0 if DEBUG else 31536000)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+    "SECURE_HSTS_INCLUDE_SUBDOMAINS",
+    default=False,
+)
+SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", default=False)
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -16,6 +54,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
 
     "rest_framework",
     "rest_framework.authtoken",
@@ -60,12 +99,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "nexo_backend.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+NEXO_DB_ENGINE = env("NEXO_DB_ENGINE").lower()
+
+if NEXO_DB_ENGINE == "spatialite":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.contrib.gis.db.backends.spatialite",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+    SPATIALITE_LIBRARY_PATH = env("SPATIALITE_LIBRARY_PATH", default="")
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.contrib.gis.db.backends.postgis",
+            "NAME": env("DB_NAME"),
+            "USER": env("DB_USER"),
+            "PASSWORD": env("DB_PASSWORD"),
+            "HOST": env("DB_HOST"),
+            "PORT": env("DB_PORT"),
+        }
+    }
 
 LANGUAGE_CODE = "es-ve"
 TIME_ZONE = "America/Caracas"
@@ -112,9 +166,20 @@ Q_CLUSTER = {
 }
 
 # --- KoBoToolbox ---
-KOBO_API_URL = os.environ.get("KOBO_API_URL", "https://kf.kobotoolbox.org/api/v2").rstrip("/")
-KOBO_TOKEN = os.environ.get("KOBO_TOKEN", "")
-KOBO_ASSET_NECESIDADES = os.environ.get("KOBO_ASSET_NECESIDADES", "")
-KOBO_ASSET_DONACIONES = os.environ.get("KOBO_ASSET_DONACIONES", "")
-KOBO_WEBHOOK_TOKEN = os.environ.get("KOBO_WEBHOOK_TOKEN", "")
-KOBO_PULL_LIMIT = int(os.environ.get("KOBO_PULL_LIMIT", "500"))
+KOBO_API_URL = env("KOBO_API_URL", default="https://kf.kobotoolbox.org/api/v2").rstrip("/")
+KOBO_TOKEN = env("KOBO_TOKEN", default="")
+KOBO_ASSET_NECESIDADES = env("KOBO_ASSET_NECESIDADES", default="")
+KOBO_ASSET_DONACIONES = env("KOBO_ASSET_DONACIONES", default="")
+KOBO_WEBHOOK_TOKEN = env("KOBO_WEBHOOK_TOKEN", default="")
+KOBO_PULL_LIMIT = env.int("KOBO_PULL_LIMIT", default=500)
+
+SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=not DEBUG)
+SESSION_COOKIE_SECURE = env.bool("SESSION_COOKIE_SECURE", default=not DEBUG)
+CSRF_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", default=not DEBUG)
+
+SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", default=0 if DEBUG else 31536000)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+    "SECURE_HSTS_INCLUDE_SUBDOMAINS",
+    default=False,
+)
+SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", default=False)
